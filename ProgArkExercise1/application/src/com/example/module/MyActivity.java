9ca713b0916cd4e09793d79ec5932885437ce4fc
rtc11 <robin.tordly@gmail.com>
2014-01-15 13:43:37 +0100

    adding the sketch for exercise 1

---
+++ ProgArkExercise1/application/src/com/example/module/MyActivity.java
@@ -0,0 +1,22 @@
+package com.example.module;
+import sheep.game.Game;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MyActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    public void onCreate(Bundle bundle){
+        super.onCreate(bundle);
+
+        //Create the game
+        Game game = new Game(this, null);
+
+        //Push the main state
+        game.pushState(new TitleScreen());
+
+        //View the game
+        setContentView(game);
+    }
+}
---
+++ ProgArkExercise1/application/src/com/example/module/TitleScreen.java
@@ -0,0 +1,9 @@
+package com.example.module;
+
+import sheep.game.State;
+
+/**
+ * Created by tordly on 15.01.14.
+ */
+public class TitleScreen extends State{
+}
