68758245a2768c7c9c8369101dea1ecad0168a27
Robin Tordly <robin.tordly@gmail.com>
2014-01-21 19:17:27 +0100

    addded task 3 module

---
+++ ProgArkExercise1/TaskThree/src/com/example/TaskThree/GameState.java
@@ -0,0 +1,91 @@
+package com.example.TaskThree;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Point;
+import android.view.Display;
+import android.view.MotionEvent;
+import android.view.WindowManager;
+import sheep.game.State;
+import sheep.game.World;
+import sheep.input.TouchListener;
+
+/**
+ * Created by tordly on 15.01.14.
+ */
+public class GameState extends State implements TouchListener {
+
+    private World world;
+    private GameLayer layer;
+    private DebugInfo debugInfo;
+    private float startX, startY, endX, endY;
+
+
+    public GameState (){
+
+        //Create the game world
+        world = new World();
+
+        //Create the layer
+        layer = new GameLayer();
+
+        //Add the layer to the world
+        world.addLayer(layer);
+
+        debugInfo = new DebugInfo(layer);
+
+        this.addTouchListener(this);
+    }
+
+    @Override
+    public void draw (Canvas canvas){
+        canvas.drawColor(Color.BLACK);
+        world.draw(canvas);
+        debugInfo.draw(canvas);
+
+        //Get the dimensions of the screen
+        Context context = getGame().getContext();
+        WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+        Display display = wm.getDefaultDisplay();
+
+        Point point = new Point();
+        display.getSize(point);
+        int width = point.x;
+        int height = point.y;
+//        Log.i("Screen size", "Width = " + width + ". Height = " + height);
+    }
+
+    @Override
+    public void update(float ft){
+        world.update(ft);
+        debugInfo.update(ft);
+    }
+
+    @Override
+    public boolean onTouchDown(MotionEvent me){
+        startX = me.getX();
+        startY = me.getY();
+        return false;
+    }
+
+    @Override
+    public boolean onTouchUp(MotionEvent me){
+
+        endX = me.getX();
+        endY = me.getY();
+        float diffX = endX - startX;
+        float diffY = endY - startY;
+
+        layer.getHelicopter().setSpeed(diffX % 30, diffY % 30);
+
+//        layer.getHelicopter().setOrientation((float)Math.atan((endX - startX)/(endY - startY)));
+
+        return false;
+    }
+
+    @Override
+    public boolean onTouchMove(MotionEvent me){
+        return false;
+    }
+}
---
+++ ProgArkExercise1/TaskThree/src/com/example/TaskThree/Wall.java
@@ -0,0 +1,24 @@
+package com.example.TaskThree;
+
+import android.graphics.Canvas;
+import android.graphics.Rect;
+import sheep.game.Sprite;
+import sheep.graphics.Color;
+
+/**
+ * Created by tordly on 17.01.14.
+ */
+public class Wall extends Sprite {
+    private Rect rectangle;
+    private Color color;
+
+    public Wall(int left, int top, int right, int bottom, Color color) {
+        rectangle = new Rect(left, top, right, bottom);
+        this.color = color;
+    }
+
+    @Override
+    public void draw(Canvas canvas) {
+        canvas.drawRect(rectangle, color);
+    }
+}
